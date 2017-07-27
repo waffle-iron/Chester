0fdaf346fc2ffbf393c9c16cef2e0e52298e1e7c
Simon Sinding <sinding2000@gmail.com>
2017-07-27 15:59:37 +0200

    added ip to history

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,8 @@
-module.exports = function () {
-  // Add your custom middleware here. Remember, that
-  // in Express the order matters
-  const app = this; // eslint-disable-line no-unused-vars
+module.exports = function() {
+    const getIp = require('./getIpOfRequest');
+    // Add your custom middleware here. Remember, that
+    // in Express the order matters
+    const app = this; // eslint-disable-line no-unused-vars
+
+    app.use(getIp);
 };
