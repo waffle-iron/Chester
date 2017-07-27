969d3b300e9d5506f61b71718a71cb45740e8d61
Simon Sinding <sinding2000@gmail.com>
2017-07-27 12:11:44 +0200

    setUser hook

---
+++ src/hooks/set-user.js
@@ -0,0 +1,13 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+module.exports = function(options = {}) {
+    // eslint-disable-line no-unused-vars
+    return function setUser(hook) {
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+        hook.data.account = hook.data.user;
+
+        return Promise.resolve(hook);
+    };
+};
