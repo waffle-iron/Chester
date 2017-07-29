def8a7e90895b97ade121c82b2591edf5e43ad4e
Simon Sinding <sinding2000@gmail.com>
2017-07-29 13:20:23 +0200

    small test

---
+++ test/validations-test/resources-validation.test.js
@@ -0,0 +1,12 @@
+const Ajv = require('ajv');
+
+const testSchema = require('../../src/schemas/tests/resources.test.json');
+const validationSchema = require('../../src/schemas/resources.validation.json');
+
+let ajv = new Ajv({ allErrors: true, $data: true });
+require('ajv-keywords')(ajv, 'select');
+
+let validator = ajv.compile(validationSchema);
+let validation = validator(testSchema);
+
+if (!validation) console.error(validator.errors);
