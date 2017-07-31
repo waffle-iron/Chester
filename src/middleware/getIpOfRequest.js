d51327537363078099b462fea8c637d0cd8a543a
Simon Sinding <sinding2000@gmail.com>
2017-07-31 11:30:11 +0200

    fixed ip of logins

--- src/middleware/getIpOfRequest.js
+++ src/middleware/getIpOfRequest.js
@@ -1,5 +1,18 @@
 module.exports = (req, res, next) => {
+    const ipregex = require('ip-regex');
+    let originIp = req.ip.match(ipregex.v4());
+
+    if (originIp !== null) {
+        req.feathers.ip = originIp[0];
+    } else {
+    // Local ?
+    // TODO: Maybe remove this else block, because it might be redundant
+    // Or maybe not, I dont know.. Ip might never be null ?! FUCK..
+    // Very confused
+        req.feathers.ip = '0.0.0.0';
+    }
+
     req.feathers.headers = req.headers;
-    req.feathers.ip = req.ip;
+
     next();
 };
